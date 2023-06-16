972e71fdf8c11384777462be830a8176a8064e57
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-16 06:22:48 +0200

    New repository

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-06-phonebook">
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-06-phonebook">
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
