48cb8a8b092dc47a9f7dd0f8900564796e0b550c
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-16 06:55:52 +0200

    Redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-06-phonebook">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
