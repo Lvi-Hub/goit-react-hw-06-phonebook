84ff4067a4bb8457da644ab45ad81fc90a8ee381
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-20 22:56:49 +0200

    New Repository

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 // persistor,
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
