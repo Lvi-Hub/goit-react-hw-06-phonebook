506ce9c9c146a48f09dfba780add6ed93d7104cb
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-06-20 19:56:49 +0200

    Done

---
+++ src/redux/store.jsx
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  // persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { clicksReducer } from './clicksSlice';
+
+export const store = configureStore({
+  reducer: {
+    clicks: clicksReducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
